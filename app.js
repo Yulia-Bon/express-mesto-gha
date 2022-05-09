ad950a729f1799d0f19dc500f2dffd6204c5cc7d
Yulia Bonohova <ybonohova@gmail.com>
2022-05-09 20:40:19 +0200

    added authorization header

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Methods', '*');
    res.header('Access-Control-Allow-Credentials', 'true');
-   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
    if (req.method === 'OPTIONS') {
      res.send(200);
    }
