dded12f93256a10da298065bbd7181b81aa24cd1
Yulia Bonohova <ybonohova@gmail.com>
2022-05-09 14:00:42 +0200

    fix

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
+const cors = require('cors');
 // const routes = require('./routes/errorsway');
 
 const { errors } = require('celebrate');
@@ -10,21 +12,44 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const { registerValid, loginValid } = require('./middlewares/validationJoi');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 // eslint-disable-next-line no-undef
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cookieParser());
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
+
+// Мидлвары
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mesto-mpozharskiy.students.nomoredomains.rocks');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger); // подключаем логгер запросов
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', registerValid, createUser);
 app.post('/signin', loginValid, login);
 
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
-
 app.use(auth);
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(routerErrorWay);
 
