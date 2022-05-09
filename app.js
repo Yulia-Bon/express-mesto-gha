4e22aa3b001a6db879afdf19d1fb344dc4006374
Yulia Bonohova <ybonohova@gmail.com>
2022-05-09 19:00:10 +0200

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-const helmet = require('helmet');
+//const helmet = require('helmet');
 
 const cors = require('cors');
 // const routes = require('./routes/errorsway');
@@ -20,30 +20,27 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // eslint-disable-next-line no-undef
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cookieParser());
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 // Мидлвары
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://mesto-express.ybon.nomoredomains.work/', 'http://localhost:3000/');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  }
-  next();
-});
+  // res.header('Access-Control-Allow-Origin', 'http://mesto-express.ybon.nomoredomains.work/', 'http://localhost:3000/');
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', '*');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   if (req.method === 'OPTIONS') {
+     res.send(200);
+   }
+   next();
+ });
+
+app.use(cookieParser());
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
-
-app.use('*', cors(options));
-app.use(helmet());
-app.use(express.json());
-app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
